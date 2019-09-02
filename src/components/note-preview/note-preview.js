4acdd6e0dc87460830d63c152ec1b09c30e8b5d5
tarthur <terzi.arthur@gmail.com>
2019-09-02 15:18:15 +0300

    added scss and modal

--- src/components/note-preview/note-preview.js
+++ src/components/note-preview/note-preview.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
+import './note-preview.scss'
+import Modal from 'react-modal';
 
-const AllNotes = props => {
-  const onClick = () => {
-    alert(1)
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+
+
+class AllNotes extends Component {
+  state = {
+    modalIsOpen: false
   }
 
-  return <div onClick={onClick}>{props.note.text} : {props.note.type}</div>
+  onClick = () => {
+    
+  }
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+
+  render() {
+    return (
+      <div className="note-preview" onClick={this.onClick}>
+        {this.props.note.text}
+          <button onClick={this.openModal}>Open Modal</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+            <button onClick={this.closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+        </Modal>
+      </div>
+    )
+  }
 }
 
 export default AllNotes
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root')
 
 ReactDOM.render(
   <App />, 
